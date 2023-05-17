136ec23b6f23b63e0b57a4d6336f46070c60a740
Nonomendi <124887163+Nonomendi@users.noreply.github.com>
2023-05-17 09:40:38 +0200

    Add files via upload

---
+++ submission-102-java-003-hangman/src/main/java/za/co/wethinkcode/examples/hangman/Player.java
@@ -0,0 +1,44 @@
+package za.co.wethinkcode.examples.hangman;
+import  java.util.Scanner;
+import java.io.InputStream;
+
+public class Player {
+    private final Scanner scanner;                                                                      
+    private int chances = 5 ;
+    private boolean quit = false;
+
+    public Player(InputStream in) {
+        this.scanner = new Scanner(in);                                                                  
+    }
+ 
+    public Player() {
+        this(System.in);       
+    }
+    public int getChances() {                                                              
+        return  this.chances ;   
+        
+    }
+
+    public void lostChance() {
+        if(!this.hasNoChances())
+            this.chances--;
+
+    }
+    public boolean hasNoChances(){
+        return this.getChances() == 0;
+    }
+
+    public String getWordsFile() {
+        String filename = scanner.nextLine();
+        return filename.isBlank() ? "short_words.txt" : filename;                                      
+    }
+
+    public String getGuess() {
+        String text = scanner.nextLine();
+        this.quit = text.equalsIgnoreCase("quit") || text.equalsIgnoreCase("exit");                                              
+        return text;
+    }
+    public boolean wantsToQuit() {                                                                      
+        return this.quit;
+    }
+}
